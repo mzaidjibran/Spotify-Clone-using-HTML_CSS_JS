43d39a119f9758226d817d43ee25b7ff2eedaf33
mzaidjibran <mzaidjibran07@gmail.com>
2026-02-04 16:53:37 +0500

    Playbar

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Let's write some JavaScript")
+console.log("Let's write some JavaScript");
+
+async function main() {
+    let songs = await getSongs();
+    console.log(songs)
+
+    //play 1st song
+
+    var audio = new Audio(songs[0]);
+    audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+        let duration = audio.duration;
+        console.log(duration)
+    });
+}
+
