fc9b6940fe89412b89957ead2f535546e027e587
Muhammad Zaid Jibran <mzaidjabran@gmail.com>
2026-02-23 15:20:37 +0500

    Error fix

--- script.js
+++ script.js
@@ -37,32 +37,7 @@ async function getSongs(folder) {
         }
 
     }
-    return songs
-}
-
-getSongs()
-
-const playMusic = (track, pause = false) => {
-    // let audio = new Audio("/songs/" + track)
-    currentSongs.src = `/${currFolder}/` + track
-    if (!pause) {
-        currentSongs.play()
-        play.src = "pause.svg"
-    }
-
-
-    document.querySelector(".songInfo").innerHTML = track
-    document.querySelector(".songTime").innerHTML = "00:00 / 00:00"
-}
-
-async function main() {
-
-    // Get the list of songs.
-
-    songs = await getSongs("songs/cs")
-    playMusic(songs[0], true)
-
-
+    // Show all the songs in the playlist
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
 
     for (const song of songs) {
@@ -89,6 +64,76 @@ async function main() {
         })
 
     })
+    return songs
+}
+
+getSongs()
+
+const playMusic = (track, pause = false) => {
+    // let audio = new Audio("/songs/" + track)
+    currentSongs.src = `/${currFolder}/` + track
+    if (!pause) {
+        currentSongs.play()
+        play.src = "pause.svg"
+    }
+
+
+    document.querySelector(".songInfo").innerHTML = track
+    document.querySelector(".songTime").innerHTML = "00:00 / 00:00"
+}
+
+async function displayAlbums() {
+    console.log("displaying albums")
+    let a = await fetch(`/songs/`)
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let anchors = div.getElementsByTagName("a")
+    let cardContainer = document.querySelector(".cardContainer")
+    let array = Array.from(anchors)
+    for (let index = 0; index < array.length; index++) {
+        const e = array[index];
+        if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
+            let folder = e.href.split("/").slice(-2)[0]
+            // Get the metadata of the folder
+            let a = await fetch(`/songs/${folder}/info.json`)
+            let response = await a.json();
+            cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
+            <div class="play">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5 20V4L19 12L5 20Z" stroke="#141B34" fill="#000" stroke-width="1.5"
+                        stroke-linejoin="round" />
+                </svg>
+            </div>
+
+            <img src="/songs/${folder}/cover.jpg" alt="">
+            <h2>${response.title}</h2>
+            <p>${response.description}</p>
+        </div>`
+        }
+    }
+
+    // Load the playlist whenever card is clicked
+    Array.from(document.getElementsByClassName("card")).forEach(e => {
+        e.addEventListener("click", async item => {
+            console.log("Fetching Songs")
+            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)
+            playMusic(songs[0])
+
+        })
+    })
+}
+
+async function main() {
+
+    // Get the list of songs.
+
+    songs = await getSongs("songs/cs")
+    playMusic(songs[0], true)
+
+
+
 
     //attach event listner to play,next and previous buttons
 
